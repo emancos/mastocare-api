0090fb9fdf0ffa07526cb8300f1b3296a1dad589
Emanuel Costa <emancos@gmail.com>
2024-04-23 20:44:37 -0300

    feat: implements User and Role entities

---
+++ src/main/java/com/api/mastocare/domain/entities/Role.java
@@ -0,0 +1,41 @@
+package com.api.mastocare.domain.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+@EntityListeners(AuditingEntityListener.class)
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    @JsonIgnore
+    private List<User> users;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+    @LastModifiedDate
+    @CreatedDate
+    @Column(insertable = false)
+    private Instant updatedAt;
+    private Instant deletedAt;
+}
