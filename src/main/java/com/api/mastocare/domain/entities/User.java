77d804951c739a8328fa654f629de57163ca91b8
Emanuel Costa <emancos@gmail.com>
2024-04-23 20:49:59 -0300

    refactor: remove unnecessary imports

--- src/main/java/com/api/mastocare/domain/entities/User.java
+++ src/main/java/com/api/mastocare/domain/entities/User.java
@@ -2,9 +2,6 @@ package com.api.mastocare.domain.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.SourceType;
-import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
