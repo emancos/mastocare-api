6ae4c6cbd1ad93ed2f747d110d0947ed4e72dd33
Emanuel Costa <emancos@gmail.com>
2024-04-24 20:56:52 -0300

    feat: add role with command line runner

--- src/main/java/com/api/mastocare/StartApplication.java
+++ src/main/java/com/api/mastocare/StartApplication.java
@@ -1,7 +1,11 @@
 package com.api.mastocare;
 
+import com.api.mastocare.domain.entities.Role;
+import com.api.mastocare.domain.repositories.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -12,6 +16,15 @@ public class StartApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StartApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner runner(RoleRepository roleRepository) {
+        return args -> {
+            if(roleRepository.findByName("ACS").isEmpty()) {
+                roleRepository.save(Role.builder().name("ACS").build());
+            }
+        };
     }
 
 }
