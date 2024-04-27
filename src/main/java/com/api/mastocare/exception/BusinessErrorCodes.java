4994915595ca29d72938f4433c7b64b8c538599f
Emanuel Costa <emancos@gmail.com>
2024-04-27 18:50:22 -0300

    feat: exception handler

---
+++ src/main/java/com/api/mastocare/exception/BusinessErrorCodes.java
@@ -0,0 +1,26 @@
+package com.api.mastocare.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.*;
+
+@Getter
+public enum BusinessErrorCodes {
+    NO_CODE(0, NOT_IMPLEMENTED, "No code"),
+    INCORRECT_CURRENT_PASSWORD(300, BAD_REQUEST, "Incorrect password is incorrect"),
+    NEW_PASSWORD_DOES_NOT_MATCH(300, BAD_REQUEST, "The new password is not match"),
+    ACCOUNT_LOCKED(302, FORBIDDEN, "User account is locked"),
+    ACCOUNT_DISABLED(303, FORBIDDEN, "User account is disabled"),
+    BAD_CREDENTIALS(304, FORBIDDEN, "Login and / or password is icorrect");
+
+    private final int code;
+    private final String description;
+    private final HttpStatus httpStatus;
+
+    BusinessErrorCodes(int code, HttpStatus httpStatus, String description) {
+        this.code = code;
+        this.description = description;
+        this.httpStatus = httpStatus;
+    }
+}
