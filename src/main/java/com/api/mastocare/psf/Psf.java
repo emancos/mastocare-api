0e3298050878520f31bdf78651c355231b574caa
Emanuel Costa <emancos@gmail.com>
2024-05-03 20:32:53 -0300

    refactor: extends BaseEntity superclass.

--- src/main/java/com/api/mastocare/psf/Psf.java
+++ src/main/java/com/api/mastocare/psf/Psf.java
@@ -1,49 +1,20 @@
 package com.api.mastocare.psf;
 
+import com.api.mastocare.common.BaseEntity;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.Instant;
-import java.util.UUID;
-
-@Data
-@Builder
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+@Getter
+@Setter
+@SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "psfs")
-@EntityListeners(AuditingEntityListener.class)
-public class Psf {
+public class Psf extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
     @Column(nullable = false, unique = true)
     private String cnes;
     @Column(length = 80, nullable = false, unique = true)
     private String name;
-    @Column(columnDefinition = "boolean default true")
-    private  boolean active;
-    @CreationTimestamp
-    @Column(nullable = false, updatable = false)
-    private Instant createdAt;
-    @UpdateTimestamp
-    @Column(insertable = false)
-    private Instant updatedAt;
-    @CreatedBy
-    @Column(nullable = false, updatable = false)
-    private Long createdBy;
-    @LastModifiedBy
-    @Column(insertable = false)
-    private Long lastModifieBy;
-
-
 }
