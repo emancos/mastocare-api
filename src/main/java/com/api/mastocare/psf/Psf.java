352c5ee32e0caed1359fb1393b1dc256a0738ebd
Emanuel Costa <emancos@gmail.com>
2024-05-03 21:08:18 -0300

    refactor: mapped relationships acs and transactions

--- src/main/java/com/api/mastocare/psf/Psf.java
+++ src/main/java/com/api/mastocare/psf/Psf.java
@@ -1,9 +1,20 @@
 package com.api.mastocare.psf;
 
 import com.api.mastocare.common.BaseEntity;
-import jakarta.persistence.*;
-import lombok.*;
+import com.api.mastocare.history.PsfTransactionalHistory;
+import com.api.mastocare.user.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+
 @Getter
 @Setter
 @SuperBuilder
@@ -17,4 +28,8 @@ public class Psf extends BaseEntity {
     private String cnes;
     @Column(length = 80, nullable = false, unique = true)
     private String name;
+    @OneToMany(mappedBy = "psf")
+    private List<User> acs;
+    @OneToMany(mappedBy = "psf")
+    private List<PsfTransactionalHistory> histories;
 }
